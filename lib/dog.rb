30c22b236b13024834e0a90cf81d6b12e59ee8d3
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-26 19:52:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
+  
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
 expect(Dog.class_variable_get(:@@all)).to match([])
+end
